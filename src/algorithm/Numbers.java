c6aaf76e6cc9219b347d2b9c2e255d775028dc66
jenad3110 <jenad.mo@gmail.com>
2022-05-18 16:39:51 -0400

    Initial commit

--- src/algorithm/Numbers.java
+++ src/algorithm/Numbers.java
@@ -1,10 +1,20 @@
 package algorithm;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Numbers {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        System.out.println("hello world");
+    Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/world/","root", "abcd123");
+        Statement stat = connection.createStatement();
+
+        String s="select * city;";
+
+        stat.execute(s);
 
     }
 
