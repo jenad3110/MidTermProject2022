14e244c5b436a8eaa0fb61e319706cc5b676317c
jenad3110 <jenad.mo@gmail.com>
2022-05-20 08:51:28 -0400

    Initial commit

---
+++ src/StringProblems/UnitTestingStringProblem.java
@@ -0,0 +1,13 @@
+package StringProblems;
+
+
+public class UnitTestingStringProblem {
+
+    public static void main(String[] args) {
+        // Unit testing for all classes within this package should be implemented here
+        System.out.println(Anagram.isAnagram("CAT","ACT"));
+        System.out.println(DetermineLargestWord.findTheLargestWord("Human brain is a biological learning machine"));
+        System.out.println(Palindrome.isPalindrome("Mom"));
+        System.out.println(Permutation.findAllPermutations("JOB"));
+    }
+}
